f1c1d08896672378423a763e1ee3f2709f40a334
Eric George <geric738@aol.com>
2019-04-19 15:29:18 -0700

    created php file to populate historical data table

--- public/api/callAPI.php
+++ public/api/callAPI.php
@@ -1,15 +1,38 @@
 <?php
 
 $key = "DA851C0JUW4Q00R0";
-$ticker = "MSFT";
+$ticker = "AMZN";
 $callType = "TIME_SERIES_DAILY";
 
+require_once("mysqlconnect.php");
+
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://www.alphavantage.co/query?function=$callType&symbol=$ticker&interval=5min&apikey=$key");
+curl_setopt($ch, CURLOPT_URL, "https://www.alphavantage.co/query?function=$callType&symbol=$ticker&outputsize=full&interval=5min&apikey=$key");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $output = curl_exec($ch);
 curl_close($ch);
 
-print($output);
+$array_output = json_decode($output, true);
+$data = $array_output["Time Series (Daily)"];
+
+foreach ($data as $key=>$value) {
+    if ($key === "2017-12-29"){
+        break;
+    }
+
+    $open = $data[$key]["1. open"];
+    $high = $data[$key]["2. high"];
+    $low = $data[$key]["3. low"];
+    $close = $data[$key]["4. close"];
+    $volume = $data[$key]["5. volume"];
+
+    $query = "INSERT INTO `stock_history` (`symbol`, `date`, `open`, `high`, `low`, `close`, `volume`)
+               VALUES ('$ticker', '$key', '$open', '$high', '$low', '$close', '$volume')"; 
+    mysqli_query($conn, $query);
+
+}
+
+
 
 ?>
 
