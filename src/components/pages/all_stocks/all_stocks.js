7ab7af222d2872b346edee0d778b6a4ab352ad76
jas-dev <jaysamora@outlook.com>
2019-04-25 11:49:47 -0700

    converted render table to fn component added state and this.state.stocks to all_stocks

--- src/components/pages/all_stocks/all_stocks.js
+++ src/components/pages/all_stocks/all_stocks.js
@@ -1,14 +1,40 @@
 import React, {Component} from 'react';
 import RenderTable from "../../render_table/render_table";
+import axios from "axios";
 
 
 class AllStocks extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            stocks: []
+        };
+    }
+    componentDidMount() {
+        this.getStockData()
+    }
+
+    getStockData(){
+        axios.get('/api/getstocks.php').then(resp=>{
+
+            this.setState({
+                stocks: resp.data.stocks
+            })
+        });
+
+    }
+
+   /* goToDetails(symbol){
+        this.props.history.push(`/stockdetails/${symbol}`)
+    }*/
+
     render(){
 
         return(
             <div className='all-stocks container'>
                 <h5 className=''>All Stocks</h5>
-                <RenderTable history={this.props.history}/>
+                <RenderTable history={this.state.stocks}/>
             </div>
         )
     }
--- src/components/render_table/render_table.js
+++ src/components/render_table/render_table.js
@@ -2,51 +2,23 @@ import React, {Component, Fragment} from 'react';
 import axios from 'axios';
 import StockListing from '../pages/all_stocks/stock_listing';
 
-class RenderTable extends Component{
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            stocks: []
-        };
-
-        console.log('state:', this.state);
-        this.goToDetails = this.goToDetails.bind(this)
-    }
-    componentDidMount() {
-        this.getStockData();
-    }
-
-    getStockData(){
-        axios.get('/api/getstocks.php').then(resp=>{
-            console.log('stock data:', resp);
-
-            this.setState({
-                stocks: resp.data.stocks
-            })
-        });
-
-    }
-    goToDetails(symbol){
-        this.props.history.push(`/stockdetails/${symbol}`)
-    }
-
-    render(){
-        const stockList = this.state.stocks.map(stocks => {
-            return(
-                <StockListing key={stocks.symbol} {...stocks} goToDetails={this.goToDetails}/>
-            )
-        });
-
-
+export default props =>{
+    
+    const stockList = this.state.stocks.map(stocks => {
         return(
-            <Fragment>
-                <ul className="collection card">
-                    {stockList}
-                </ul>
-            </Fragment>
+            <StockListing key={stocks.symbol} {...stocks} goToDetails={this.goToDetails}/>
         )
-    }
+    });
+
+
+    return(
+        <Fragment>
+            <ul className="collection card">
+                {stockList}
+            </ul>
+        </Fragment>
+    )
+
 }
 
-export default RenderTable;
+
