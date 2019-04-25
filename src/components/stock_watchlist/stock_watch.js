86ac083fdd6b402151ac1b0c7ca9880cb6f76700
jas-dev <jaysamora@outlook.com>
2019-04-25 15:55:40 -0700

    commented out table component where nested in other pages for the time being starting work on refactoring watchlist table call

--- src/components/stock_watchlist/stock_watch.js
+++ src/components/stock_watchlist/stock_watch.js
@@ -1,12 +1,12 @@
 import React, {Component, Fragment} from 'react';
-import Table from '../render_table/render_table'
+import RenderTable from '../render_table/render_table'
 
 class Watchlist extends Component{
     render(){
         return(
            <div className='container'>
                 <h5 className=''>Watchlist</h5>
-                <Table/>
+                {/*<RenderTable/>*/}
            </div>
         )
     }
