f479d21db8e1f0bcd44b4c3b82d8666ff9efb7e6
jas-dev <jaysamora@outlook.com>
2019-04-25 18:37:38 -0700

    converted transactions to class component passed data to tablerender and got transactions table displayed

--- src/components/pages/transactions/transactions.js
+++ src/components/pages/transactions/transactions.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import RenderTable from '../../render_table/render_table'
 
-export default props =>{
-    return (
-        <div className='container'>
-            <h5>Transactions</h5>
-            {/*<RenderTable/>*/}
-        </div>
-    )
+class Transactions extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            stocks: []
+        }
+
+    }
+
+    componentDidMount() {
+        this.getStockData()
+    }
+
+    getStockData(){
+        axios.get('/api/gettransactions.php').then(resp=>{
+
+            this.setState({
+                stocks: resp.data.transactions
+            })
+        });
+    }
+
+    render(){
+        if(!this.state.stocks.length){
+            return null;
+        }
+
+        return (
+            <div className='container'>
+                <h5>Transactions</h5>
+                <RenderTable stocks={this.state.stocks}/>
+            </div>
+        )
+
+    }
+
 }
+
+export default Transactions;
