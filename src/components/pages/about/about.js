6f85bb6230660376c1ed31ddcc8d903cf511b7ff
jas-dev <jaysamora@outlook.com>
2019-04-29 12:01:15 -0700

    changed name back to investmentbuddy on nav and about

--- src/components/pages/about/about.js
+++ src/components/pages/about/about.js
@@ -8,7 +8,7 @@ export default props =>{
         <div className='about-wrapper container'>
             <h1 className='center'> About</h1>
             <div className='about'>
-                <span>Welcome to MoneyTrainer! We retrieve live stock prices and historical data so you can learn to make trading decisions! Simulate trades with your available funds and watch your profits rise!</span>
+                <span>Welcome to InvestmentBuddy! We retrieve live stock prices and historical data so you can learn to make trading decisions! Simulate trades with your available funds and watch your profits rise!</span>
 
 
             </div>
