4d0539b8617a9fc95ce9b5ee0fe1bfaba9237e72
jas-dev <jaysamora@outlook.com>
2019-05-16 11:33:45 -0700

    worked with scott to fix sign in removed about heading

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,26 @@
 import axios from "axios";
 
-export function signIn() {
-    return {
-        type: 'LOG_USER_IN'
-    };
+export function signIn(values) {
+    return async function (dispatch){
+        try {
+            const resp = await axios.post('/api/login.php', values);
+
+            if (resp.data.success) {
+                localStorage.setItem("investmentBuddy", resp.data.userData.token);
+
+                dispatch({
+                    type: 'LOG_USER_IN'
+                });
+
+                return true;
+            }
+
+            throw new Error('Error signing in');
+
+        } catch(err){
+            return false
+        }
+    }
 }
 
 export function signOut() {
