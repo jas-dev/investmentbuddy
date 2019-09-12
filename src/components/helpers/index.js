06af05ceae47cf5a1473f64291152b9362600b18
jas-dev <jaysamora@outlook.com>
2019-09-12 15:28:34 -0700

    reformated spaces for better readability fixed typo in string urlealized to unrealized removed unused import statements removed unused functions

--- src/components/helpers/index.js
+++ src/components/helpers/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import RenderTable from '../render_table/render_table';
-import { COPYFILE_FICLONE_FORCE } from 'constants';
-
 
 export function formatMarketIndex(stocks) {
     const linesData = [];
@@ -11,7 +8,7 @@ export function formatMarketIndex(stocks) {
         const xData = Object.keys(symbolData);
         const yData = [];
 
-        for (let index=0; index<xData.length; index++){
+        for (let index = 0; index < xData.length; index++){
             yData.push(parseFloat(stocks.prices[symbolIndex][xData[index]]));
         }
 
@@ -28,20 +25,20 @@ export function formatHistory(stock, type){
 
     let xData = [];
     let yData = [];
-    if (type==="price"){
+    if (type === "price"){
         xData = Object.keys(stock.priceHistory);
-    } else if (type==="percent"){
+    } else if (type === "percent"){
         xData = Object.keys(stock.percentHistory);
     }
     let reorderXData = [];
-    for (let reorderIndex=xData.length-1; reorderIndex>=0; reorderIndex--){
+    for (let reorderIndex = xData.length-1; reorderIndex >= 0; reorderIndex--){
         reorderXData.push(xData[reorderIndex]);
     }
 
-    for (let index=0; index<reorderXData.length; index++){
-        if (type==="price"){
+    for (let index = 0; index < reorderXData.length; index++){
+        if (type === "price"){
             yData.push(parseFloat(stock.priceHistory[reorderXData[index]]));
-        } else if (type==="percent"){
+        } else if (type === "percent"){
             yData.push(parseFloat(stock.percentHistory[reorderXData[index]]));
         }
     }
@@ -58,13 +55,13 @@ export function formatHistory(stock, type){
 }
 
 export function formatHeaders(str){
-    if (str===""){
+    if (str === ""){
         return str;
-    } else if (str==="timestamp"){
+    } else if (str === "timestamp"){
         return "Last Updated";
-    } else if (str==="unrealized"){
-        return "Urealized P/L";
-    } else if (str==="realizedPl"){
+    } else if (str === "unrealized"){
+        return "Unrealized P/L";
+    } else if (str === "realizedPl"){
         return "Realized P/L";
     }
     const arr = str.split("");
@@ -72,12 +69,12 @@ export function formatHeaders(str){
 
     firstLetter = firstLetter.toUpperCase();
     arr[0] = firstLetter;
-    for (let index=1; index<arr.length; index++){
-        if (arr[index]===arr[index].toUpperCase() && arr[index]!=="_"){
+    for (let index=1; index < arr.length; index++){
+        if (arr[index] === arr[index].toUpperCase() && arr[index] !== "_"){
             arr.splice(index, 0, " ");
             break;
-        } else if (arr[index]==="_"){
-            arr[index]=" ";
+        } else if (arr[index] === "_"){
+            arr[index] = " ";
             arr[index+1] = arr[index+1].toUpperCase();
             break;
         }
@@ -86,12 +83,8 @@ export function formatHeaders(str){
     return arr.join("");
 }
 
-export function convertAccountData(props){
-    const {availBalance,totalAsset,availToTrade} = props;
-}
-
 export function formatDateTime(str){
-    if (str===null){
+    if (str === null){
         return "N/A";
     }
     const arr = str.split(" ");
@@ -99,7 +92,7 @@ export function formatDateTime(str){
     const dateString = arr[0];
     const dateArray = dateString.split("-");
     
-    if (dateArray[1][0]==="0"){
+    if (dateArray[1][0] === "0"){
         dateArray[1] = dateArray[1][1];
     }
     let formattedDateString = dateArray[1]+"/"+dateArray[2]+"/"+dateArray[0];
@@ -109,23 +102,22 @@ export function formatDateTime(str){
         return formattedDateString;
     } else {
     timeString = arr[1]; }
-    if (timeString==="00:00:00"){
+    if (timeString === "00:00:00"){
         return formattedDateString;
     }
     const timeArray = timeString.split(":");
-    if (timeArray[0]>12){
-        timeArray[0]-=12;
+    if (timeArray[0] > 12){
+        timeArray[0] -= 12;
     }
     formattedDateString += (", "+timeArray[0]+":"+timeArray[1]+":"+timeArray[2]);
 
     return formattedDateString;
 }
 
-
 export function randomizeStocks(arr){
     let result = [];
-    for (let index=0; index<arr.length; index++){
-        let randomStock = Math.floor(Math.random()*(arr.length-1));
+    for (let index = 0; index < arr.length; index++){
+        let randomStock = Math.floor(Math.random() * (arr.length-1));
         result.push(arr[randomStock]);
         arr.slice(index, 1);
     }
@@ -139,17 +131,17 @@ export function moneyCommas(num){
 
     let decimal = false;
     let startIndex = arr.length-1;
-    for (let index=0; index<arr.length; index++){
-        if (arr[index]==="."){
+    for (let index = 0; index < arr.length; index++){
+        if (arr[index] === "."){
             decimal = true;
-            startIndex=index-1;
+            startIndex = index-1;
         }
     }
 
     let counter = 1;
  
-    for (let index=startIndex; index>=0; index--){
-        if (counter%3===0 && (arr[index-1]!=="-"&&typeof arr[index-1]!=="undefined")){
+    for (let index = startIndex; index >= 0; index--){
+        if (counter%3 === 0 && (arr[index-1] !== "-" && typeof arr[index-1] !== "undefined")){
             arr.splice(index, 0, ",");
         }
         counter++;
@@ -158,7 +150,6 @@ export function moneyCommas(num){
     const result = arr.join("");
     return result;
 }
-
 
 export function formatNegativeMoney(num){
     let str = num.toString();
